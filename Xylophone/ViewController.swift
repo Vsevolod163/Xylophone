16e599afff54efc0f62dd611a5a305f5bcde0366
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-10 02:23:13 +0300

    Corner radius to views added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var soundViews: [UIView]!
+    
+    private var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        soundViews.forEach { soundView in
+            soundView.layer.cornerRadius = 10
+        }
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
