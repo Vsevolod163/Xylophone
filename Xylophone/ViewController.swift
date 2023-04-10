29b7595e986cc501d60d9ea91c9b9bf1d4ab0e24
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-10 03:31:39 +0300

    logic and sounds added

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,22 +9,36 @@ import UIKit
 import AVFoundation
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var soundViews: [UIView]!
     
-    private var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         soundViews.forEach { soundView in
             soundView.layer.cornerRadius = 10
         }
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(with: sender.titleLabel?.text ?? "")
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(with sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
         
     }
     
 }
+
 
